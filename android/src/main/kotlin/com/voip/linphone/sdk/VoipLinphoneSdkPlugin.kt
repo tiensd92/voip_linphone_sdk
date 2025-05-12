7e5618dc3f9da6a1cacd2287fa8a77e260bb7415
Dino <dino@192.168.2.6>
2025-05-12 09:16:07 +0700

    integrate android

--- android/src/main/kotlin/com/voip/linphone/sdk/VoipLinphoneSdkPlugin.kt
+++ android/src/main/kotlin/com/voip/linphone/sdk/VoipLinphoneSdkPlugin.kt
@@ -1,15 +1,14 @@
 package com.voip.linphone.sdk
 
-import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** VoipLinphoneSdkPlugin */
-class VoipLinphoneSdkPlugin: FlutterPlugin, MethodCallHandler {
+class VoipLinphoneSdkPlugin: FlutterPlugin, MethodCallHandler, EventChannel.StreamHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
@@ -19,6 +18,9 @@ class VoipLinphoneSdkPlugin: FlutterPlugin, MethodCallHandler {
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "voip_linphone_sdk")
     channel.setMethodCallHandler(this)
+
+    val eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, "voip_linphone_sdk_event_channel")
+    eventChannel.setStreamHandler(this)
   }
 
   override fun onMethodCall(call: MethodCall, result: Result) {
@@ -31,5 +33,13 @@ class VoipLinphoneSdkPlugin: FlutterPlugin, MethodCallHandler {
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+  }
+
+  override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+    
+  }
+
+  override fun onCancel(arguments: Any?) {
+
   }
 }
